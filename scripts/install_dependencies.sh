c4b132c10ee3ba69d6ba9970045d854f775312ff
전상현 <sanghyunjun@jeonsanghyeon-ui-MacBookPro.local>
2025-03-24 17:28:49 +0900

    Fix: install nodejs in dependeices script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /home/ec2-user/backend
 
-sudo chown -R ec2-user:ec2-user /home/ec2-user/backend
+# Node.js 16.x 설치
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+
+# 그 외 의존성 설치
 npm install
